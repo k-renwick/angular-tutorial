46aefc968f9b7c46cbb866ac6dcd510d69365464
k-renwick <katrina.renwick@ratehub.ca>
2025-10-21 01:03:56 -0400

    create and import product-alert component

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
-  standalone: true,
-  imports: [],
   templateUrl: './product-alerts.component.html',
-  styleUrl: './product-alerts.component.css'
+  styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent {
 
